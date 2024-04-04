e48a3b73bef6575254a157b1773ec235e10b8da3
Arthur Shaimardanov <arthur.shaimardanov@sofoil.com>
2024-04-04 23:11:07 +0300

    add check_winner

--- tic_tac_toe_game/logic.py
+++ tic_tac_toe_game/logic.py
@@ -2,27 +2,59 @@ class TicTacToe:
     def __init__(self):
         self.size = 15
         self.board = [None] * self.size ** 2
+        self.__game_win_lenght = 5
 
-    def check_winner(self, x, y, player):
-        """
-        Проверяет, победил ли игрок после совершения хода.
-        """
-        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]  # вертикаль, горизонталь, две диагонали
-        for dx, dy in directions:
-            count = 1
-            for dir in [1, -1]:  # Проверяем в обе стороны от последнего хода
-                step = 1
-                while True:
-                    nx, ny = x + step * dx * dir, y + step * dy * dir
-                    if 0 <= nx < self.size and 0 <= ny < self.size:
-                        index = nx + ny * self.size
-                        if self.board[index] == player:
-                            count += 1
-                            if count == 5:  # Нужно 5 подряд для победы
-                                return True
-                        else:
-                            break
-                    else:
-                        break
-                    step += 1
+    def get_decart_coordinate(self, ind):
+        """Return x, y coordinate."""
+        return ind // self.size, ind % self.size - 1
+
+    def get_index_from_coordinate(self, x, y):
+        return x * 15 + y
+
+    def check_winner(self, player, ind):
+        cur_x, cur_y = self.get_decart_coordinate(ind)
+        going_up, going_down, k, counter = True, True, 0, 0
+
+        while going_up or going_down:
+            if going_up and self.get_index_from_coordinate(cur_x + k, cur_y) == 'X':
+                counter += 1
+            else:
+                going_up = False
+            if going_down and self.get_index_from_coordinate(cur_x - k, cur_y) == 'X':
+                counter += 1
+            else:
+                going_down = False
+            k += 1
+
+        if counter >= self.__game_win_lenght:
+            return True
+
+        while going_up or going_down:
+            if going_up and self.get_index_from_coordinate(cur_x, cur_y + k) == 'X':
+                counter += 1
+            else:
+                going_up = False
+            if going_down and self.get_index_from_coordinate(cur_x, cur_y - k) == 'X':
+                counter += 1
+            else:
+                going_down = False
+            k += 1
+
+        if counter >= self.__game_win_lenght:
+            return True
+
+        while going_up or going_down:
+            if going_up and self.get_index_from_coordinate(cur_x + k, cur_y + k) == 'X':
+                counter += 1
+            else:
+                going_up = False
+            if going_down and self.get_index_from_coordinate(cur_x - k, cur_y + k) == 'X':
+                counter += 1
+            else:
+                going_down = False
+            k += 1
+
+        if counter >= self.__game_win_lenght:
+            return True
+
         return False
